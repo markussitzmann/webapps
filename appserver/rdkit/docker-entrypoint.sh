71537204bb5cf9c3ef2991e5e52cf28c9a19d83d
Markus Sitzmann <markus.sitzmann@gmail.com>
2016-09-11 01:09:34 +0200

    clean up and adjust postgres and postgres/rdkit start up mechanism to be more similar; separation into appserver user and postgres user; databases are written to volumes; in working conditions

--- appserver/rdkit/docker-entrypoint.sh
+++ appserver/rdkit/docker-entrypoint.sh
@@ -16,24 +16,20 @@ if [ "$1" = 'postgres' ]; then
     source activate appserver
 
 	mkdir -p "$PGDATA"
-	#chown -R postgres "$PGDATA"
-
-	#chmod g+s /run/postgresql
-	#chown -R postgres /run/postgresql
 
 	# look specifically for PG_VERSION, as it is expected in the DB dir
 	if [ ! -s "$PGDATA/PG_VERSION" ]; then
 
-	    initdb
+	    initdb --username postgres
 
-    	# check password first so we can output the warning before postgres
-    	# messes it up
-	    if [ "$POSTGRES_PASSWORD" ]; then
-		    pass="PASSWORD '$POSTGRES_PASSWORD'"
-		    authMethod=md5
-	    else
-		    # The - option suppresses leading tabs but *not* spaces. :)
-		    cat >&2 <<-'EOWARN'
+		# check password first so we can output the warning before postgres
+		# messes it up
+		if [ "$POSTGRES_PASSWORD" ]; then
+			pass="PASSWORD '$POSTGRES_PASSWORD'"
+			authMethod=md5
+		else
+			# The - option suppresses leading tabs but *not* spaces. :)
+			cat >&2 <<-'EOWARN'
 				****************************************************
 				WARNING: No password has been set for the database.
 				         This will allow anyone with access to the
@@ -47,49 +43,47 @@ if [ "$1" = 'postgres' ]; then
 				****************************************************
 			EOWARN
 
-		    pass=
-		    authMethod=trust
-	    fi
+			pass=
+			authMethod=trust
+		fi
 
-	    { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
+		{ echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
 
-	    # internal start of server in order to allow set-up using psql-client
-	    # does not listen on TCP/IP and waits until start finishes
-	    pg_ctl -D "$PGDATA" \
-		    -o "-c listen_addresses=''" \
-		    -w start
+		# internal start of server in order to allow set-up using psql-client
+		# does not listen on TCP/IP and waits until start finishes
+		pg_ctl -D "$PGDATA" \
+			-o "-c listen_addresses=''" \
+			-w start \
+			-U postgres
 
-	    : ${POSTGRES_USER:=postgres}
-	    : ${POSTGRES_DB:=$POSTGRES_USER}
-	    export POSTGRES_USER POSTGRES_DB
+		: ${POSTGRES_USER:=appserver}
+		: ${POSTGRES_DB:=$POSTGRES_USER}
+		export POSTGRES_USER POSTGRES_DB
 
-	    if [ "$POSTGRES_DB" != 'postgres' ]; then
-		    createdb ${POSTGRES_DB}
-	    fi
-
-	    psql --username postgres <<-EOSQL
-			ALTER USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
+		psql --username postgres <<-EOSQL
+			CREATE USER appserver WITH SUPERUSER $pass ;
 		EOSQL
-	    echo
-	
-	    echo
-	    for f in /docker-entrypoint-initdb.d/*; do
-		    case "$f" in
-			    *.sh)  echo "$0: running $f"; . "$f" ;;
-			    *.sql) echo "$0: running $f"; psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < "$f" && echo ;;
-			    *)     echo "$0: ignoring $f" ;;
-		    esac
-		    echo
-	    done
+		echo
 
-	    pg_ctl -D "$PGDATA" -m fast -w stop
-	    set_listen_addresses '*'
+        createdb
 
-	    echo
-	    echo 'PostgreSQL init process complete; ready for start up.'
-	    echo
+		echo
+		for f in /docker-entrypoint-initdb.d/*; do
+			case "$f" in
+				*.sh)  echo "$0: running $f"; . "$f" ;;
+				*.sql) echo "$0: running $f"; psql --username appserver --dbname appserver < "$f" && echo ;;
+				*)     echo "$0: ignoring $f" ;;
+			esac
+			echo
+		done
+
+		pg_ctl -D "$PGDATA" -m fast -w stop -U postgres
+		set_listen_addresses '*'
+
+		echo
+		echo 'PostgreSQL init process complete; ready for start up.'
+		echo
 	fi
-
 fi
 
 if [ "$1" = 'python' ]; then
