e7b4e346f0494f98e243f3c8c43020cdc6d977c9
Markus Sitzmann <markus.sitzmann@gmail.com>
2017-01-02 15:49:17 +0100

    switching to elasticsearch new Docker format; successful test built

--- appserver/elasticsearch/2.4/alpine/docker-entrypoint.sh
+++ appserver/elasticsearch/2.4/alpine/docker-entrypoint.sh
@@ -10,17 +10,16 @@ fi
 # Drop root privileges if we are running elasticsearch
 # allow the container to be started with `--user`
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
-
-    mkdir -p "$INDEXDIR"
-
-	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	#chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-	chown -R appserver:appserver $INDEXDIR
-	chmod -R 777 $INDEXDIR
+	# Change the ownership of user-mutable directories to elasticsearch
+	for path in \
+		/usr/share/elasticsearch/data \
+		/usr/share/elasticsearch/logs \
+	; do
+		chown -R elasticsearch:elasticsearch "$path"
+	done
 	
-	set -- gosu elasticsearch "$@"
-	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
-
+	set -- su-exec elasticsearch "$@"
+	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
 fi
 
 # As argument is not related to elasticsearch,
